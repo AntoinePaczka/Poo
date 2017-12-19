bf95ee3dc96489084cff7b48756827990cffb73a
Antoine Paczka <antoine.paczka@devinci.fr>
2017-12-19 21:57:22 +0100

    Initial commit

---
+++ src/Str.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace String;
+
+
+class Str
+{
+    private $string;
+
+    public static function on($string)
+    {
+        return new Str($string);
+    }
+
+    public function __construct($string)
+    {
+        $this->string = $string;
+    }
+
+    public function replace($search, $replace)
+    {
+        $this->string = str_replace($search,$replace, $this->string);
+
+        return $this;
+    }
+
+    public function ucwords()
+    {
+        $this->string = ucwords($this->string);
+        return $this;
+    }
+
+    public function lcfirst()
+    {
+        $this->string = lcfirst($this->string);
+        return $this;
+    }
+
+    public function strlower(){
+        $this->string = strtolower($this->string);
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->string;
+    }
+
+}
