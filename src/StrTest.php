decd9925518cfa1a4d089b0717a919407953fe7d
Antoine Paczka <antoine.paczka@devinci.fr>
2017-12-19 22:04:56 +0100

    Exo1

--- src/StrTest.php
+++ src/StrTest.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace String;
+
+use PHPUnit\Framework\TestCase;
+
+class StackTest extends TestCase
+{
+    public function testExo1()
+    {
+        $string = (string) Str::on('my_string')
+            ->replace('_', ' ')
+            ->ucwords()
+            ->replace(' ', '')
+            ->lcfirst();
+
+        $this->assertSame('myString', $string);
+
+    }
+}
+?>
